261cfbfcf6ca2906cda51846cab2d7fa4d0c22ce
dirtflare <dirtflare@gmail.com>
2022-04-19 02:33:35 +0900

    assignment02

---
+++ main.js
@@ -0,0 +1,16 @@
+document.getElementById("start").addEventListener("click", function () {
+    location.replace("https://dirtflare.github.io/jankenpon.io/");
+});
+document.getElementById("start2").addEventListener("click", function () {
+    location.replace("https://dirtflare.github.io/quiz.io/");
+});
+
+
+
+// ゲームスタートボタンをクリック
+// $('#startButton a').on('click', function () {
+//     $(this).blur();
+
+//     $('pageStart').hide();
+//     $('pagePlaying').show();
+// })
